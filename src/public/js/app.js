bdeb7adfbcc4b1805baa401c08186825d8d96e69
unknown <5251sun@gmail.com>
2021-08-31 11:57:01 +0900

    #1-2

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
@@ -8,10 +10,24 @@ socket.addEventListener("message", (message) => {
   console.log(message.data);
 });
 
+// socket.addEventListener('message', async (message) => {
+//   if (typeof message.data === 'string') {
+//   console.log(`New message :`, message.data);
+//   } else {
+//   const messageText = await message.data.text();
+//   console.log(messageText);
+//   }
+//   });
+
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server");
 });
 
-setTimeout(() => {
-  socket.send("Hello from the otherside");
-}, 5000);
+function handleSunmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSunmit);
