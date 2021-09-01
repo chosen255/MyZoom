ec6d74994f3b75eabd3cd2a8226fe396121fabae
unknown <5251sun@gmail.com>
2021-09-01 12:24:53 +0900

    #1-3

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,21 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log(message.data);
+  li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 // socket.addEventListener('message', async (message) => {
@@ -26,8 +34,16 @@ socket.addEventListener("close", () => {
 function handleSunmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
   input.value = "";
 }
 
 messageForm.addEventListener("submit", handleSunmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -20,13 +20,24 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "UnknownUser";
   console.log("Connected to Browser");
   socket.on("close", () => {
     console.log("Disconnected from the Browser");
   });
-  socket.on("message", (message, isBinary) => {
-    const messageString = isBinary ? message : message.toString("utf8");
-    sockets.forEach((aSocket) => aSocket.send(messageString));
+  socket.on("message", (msg, isBinary) => {
+    const messageString = isBinary ? msg : msg.toString("utf8");
+    const message = JSON.parse(messageString);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
   });
 });
 
